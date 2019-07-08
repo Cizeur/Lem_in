06b4e620b6c5a76c08e64ddf6961c1803de66476
Cristina FERNANDEZ <crfernan@e1r4p18.42.fr>
2019-07-08 12:59:42 +0200

    ft_parse_check_pipe.c

---
+++ lemin/src/ft_parse_check_pipe.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_parse_check_pipe.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: crfernan <crfernan@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/07/08 11:59:16 by crfernan          #+#    #+#             */
+/*   Updated: 2019/07/08 12:56:20 by crfernan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "lem_in.h"
+#include "libft/libft.h"
+#include "utils.h"
+
+t_ln_type		ft_parser_check_pipe(char *line, t_ln_type type)
+{
+	int		i;
+	int		pipe;
+	int		check_first_name;
+	int		check_second_name;
+
+	i = 0;
+	pipe = 0;
+	check_first_name = 0;
+	check_second_name = 0;
+	while (line[i] && (ft_is_whitespace(line[i]) || line[i] == "-") && pipe < 2)
+	{
+		if (ft_is_whitespace(line[i]) && pipe == 0)
+			check_first_name++;
+		if (line[i] == "-")
+			pipe++;
+		if (ft_is_whitespace(line[i]) && pipe == 1)
+			check_second_name++;
+		i++;
+	}
+	if (!(line[i]) && pipe == 1 && check_first_name && check_second_name)
+		type = PIPE;
+	return (type);
+}
