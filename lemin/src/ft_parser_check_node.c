780e69cbb87d359d5979b218c57def1aca214e89
Cizeur <cgiron@e1r5p17.42.fr>
2019-07-08 13:04:40 +0200

    entry filling in order

--- lemin/src/ft_parser_check_node.c
+++ lemin/src/ft_parser_check_node.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_parser_check_cell.c                             :+:      :+:    :+:   */
+/*   ft_parser_check_node.c                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: cgiron <cgiron@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/03 09:18:05 by cgiron            #+#    #+#             */
-/*   Updated: 2019/07/08 11:34:20 by cgiron           ###   ########.fr       */
+/*   Updated: 2019/07/08 13:04:13 by cgiron           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 #include "libft/libft.h"
 #include "utils.h"
 
-char	*ft_check_numbers(char *line, int *i_adr)
+static char		*ft_check_numbers(char *line, int *i_adr)
 {
 	char	*pos;
 	int		i;
@@ -39,7 +39,17 @@ char	*ft_check_numbers(char *line, int *i_adr)
 	*i_adr = i;
 	return (pos);
 }
-
+/*
+static int		ft_one_word(char *start, char *end)
+{
+	while (*start && start != end)
+	{
+		if (ft_is_whitespace(*start))
+			return (0);
+	}
+	return(1);
+}
+*/
 t_ln_type		ft_parser_check_node(char *line, t_ln_type type)
 {
 	int i;
@@ -52,7 +62,9 @@ t_ln_type		ft_parser_check_node(char *line, t_ln_type type)
 	if ((pos[POS_Y] = ft_check_numbers(line, &i)))
 	{
 		if ((pos[POS_X] = ft_check_numbers(line, &i)))
-			type = CELL;
+		{
+			type = NODE;
+		}
 	}
 	return(type);
 }
