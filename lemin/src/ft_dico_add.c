e944a351de1d2ee2f0c14125e3169964fc1f4bb2
Cizeur <cgiron@e1r5p17.42.fr>
2019-07-08 16:37:48 +0200

    working dictionnary

--- lemin/src/ft_dico_add.c
+++ lemin/src/ft_dico_add.c
@@ -6,25 +6,40 @@
 /*   By: cgiron <cgiron@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/03 14:28:35 by cgiron            #+#    #+#             */
-/*   Updated: 2019/07/08 15:27:20 by cgiron           ###   ########.fr       */
+/*   Updated: 2019/07/08 16:36:36 by cgiron           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lem_in.h"
 #include "libft/libft.h"
-/*
-static int	ft_dico_check_double_inc(t_master *mstr,
+
+
+static t_bucket	ft_dico_check_double_inc(t_master *mstr,
 				t_hash_dico *dico,t_line_info *entry)
 {
-	t_bucket dico_entry;
+	t_bucket *dico_entry;
 	t_bucket *bucket;
+	t_bucket *tmp;
+	t_line_info *cmp_line;
 
-	dico_entry = dico->dico_list[entry->hash_key];
-	bucket = dico_entry.next;
-	if (ft_strncmp())
-
+	dico_entry = &(dico->dico_list[entry->hash_key]);
+	bucket = dico_entry;
+	while (bucket)
+	{
+		cmp_line = ft_storage_get_line(mstr->storage_start, bucket->line_index);
+		if (entry->name_len == cmp_line->name_len &&
+			!ft_strncmp(entry->line, cmp_line->line, cmp_line->name_len))
+			ft_exit(FAIL_REPEAT_ENTRY);
+		tmp = bucket;
+		bucket = bucket->next;
+	}
+	if (!(bucket = (t_bucket *)ft_memalloc(sizeof(t_bucket))))
+		ft_exit(FAIL_MALLOC_DIC);
+	tmp->next = bucket;
+	bucket->line_index = entry->line_index;
+	return (*dico_entry);
 }
-*/
+
 int			ft_dico_add(t_master *mstr, t_hash_dico *dico,t_line_info *entry)
 {
 	t_bucket dico_entry;
@@ -32,8 +47,11 @@ int			ft_dico_add(t_master *mstr, t_hash_dico *dico,t_line_info *entry)
 	dico_entry = dico->dico_list[entry->hash_key];
 	if (!dico_entry.line_index)
 		dico_entry.line_index = entry->line_index;
-//	else
-//		ft_dico_check_double(mstr, dico, entry);
+	else
+	{
+		dico_entry = ft_dico_check_double_inc(mstr, dico, entry);
+		dico->collisions++;
+	}
 	dico->dico_list[entry->hash_key] =  dico_entry;
 	return(1);
 	mstr= 0;
--- lemin/src/parser.c
+++ lemin/src/parser.c
@@ -6,7 +6,7 @@
 /*   By: cgiron <cgiron@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/02 14:35:16 by cgiron            #+#    #+#             */
-/*   Updated: 2019/07/08 15:24:25 by cgiron           ###   ########.fr       */
+/*   Updated: 2019/07/08 16:26:45 by cgiron           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,13 +32,13 @@ void 		ft_storage_print(t_storage *storage, int ind_max)
 		length = ft_strlen(entry->line);
 		//ft_putstr(entry->line);
 		//ft_putchar('\n');
-		printf("||%s||- type %d - index %d ",
-		entry->line, entry->type, entry->line_index);
-		if (entry->type == NODE)
-			printf("node_nb %d - name [[%.*s]] -hash %d",
-				entry->node_number ,entry->name_len
-				,entry->line, entry->hash_key);
-		printf("\n");
+	//	printf("||%s||- type %d - index %d ",
+	//	entry->line, entry->type, entry->line_index);
+	//	if (entry->type == NODE)
+	//		printf("node_nb %d - name [[%.*s]] -hash %d",
+	//			entry->node_number ,entry->name_len
+	//			,entry->line, entry->hash_key);
+	//	printf("\n");
 	}
 }
 
@@ -62,4 +62,5 @@ void		parser(t_master *mstr)
 	if (r == -1)
 		ft_exit(FAIL_ON_READ);
 	ft_storage_print(mstr->storage_start, mstr->lines_nb);
+	printf("collisions : %d\n", mstr->dico->collisions);
 }
