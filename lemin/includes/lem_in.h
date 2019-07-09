9c7807a233f8713a1479ae6ebc7ea517af49d6d2
Cizeur <cgiron@e1r5p17.42.fr>
2019-07-09 08:55:15 +0200

    command management

--- lemin/includes/lem_in.h
+++ lemin/includes/lem_in.h
@@ -6,7 +6,7 @@
 /*   By: cgiron <cgiron@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/18 14:44:24 by cgiron            #+#    #+#             */
-/*   Updated: 2019/07/08 18:18:34 by cgiron           ###   ########.fr       */
+/*   Updated: 2019/07/09 08:43:37 by cgiron           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,10 +34,16 @@
 # define POS_X 1
 # define POS_Y 2
 /*
-** LINE DEFINES
+** PIPE DEFINES
 */
 # define NOPE 0
 # define CERTAINLY 1
+/*
+** COMMAND DEFINES
+*/
+# define COM_DEFAULT 0
+# define COM_START 1
+# define COM_END 2
 /*
 ** COMMAND LINES
 */
@@ -97,6 +103,7 @@ typedef struct	s_master
 {
 	int				ants_nb;
 	int				nodes_nb;
+	int				command_line;
 	int				**adjacency_mtx;
 	t_storage		*storage;
 	t_storage		*storage_start;
@@ -123,6 +130,7 @@ void				ft_parser_fill_entry_node(t_master *mstr, char *line,
 						t_line_info *entry);
 void				ft_parser_fill_entry_pipe(t_master *mstr, char *line,
 						t_line_info *entry);
+void				ft_parser_apply_command(t_master *mstr, t_line_info *entry);
 
 void				ft_storage_grow(t_master *mstr);
 t_ln_type 			ft_storage_add_line(char *line, t_master *mstr);
