6ca837d6ffa2523dfcaee7a86e9b597c06bbd94e
Cizeur <cgiron@e1r1p14.42.fr>
2019-07-12 15:24:10 +0200

    separation of debug print functions

---
+++ lemin/includes/debug.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   debug.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cgiron <cgiron@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/07/12 13:55:55 by cgiron            #+#    #+#             */
+/*   Updated: 2019/07/12 15:21:27 by cgiron           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef DEBUG_H
+# define DEBUG_H
+
+
+#define DEBUG_PRINT_MATRIX 1
+#define DEBUG_PRINT_TEST_RESULT 0
+#define DEBUG_PRINT_STORAGE 0
+
+#include "lem_in.h"
+void    ft_print_matrix(t_master *mstr, int activation);
+void 	ft_debug_storage_print(t_storage *storage, int ind_max, int activation);
+void    ft_print_test(t_master *mstr, int activation);
+#endif
