049156c95c7de40c6592d4e57b59f986738d187c
Cesar <cesar@iMac-de-famille.local>
2019-07-17 11:04:10 +0200

    new output header for the visual

---
+++ lemin/includes/output.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   output.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cesar <cesar@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/07/17 10:58:52 by cesar             #+#    #+#             */
+/*   Updated: 2019/07/17 11:02:26 by cesar            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef OUTPUT_H
+# define OUTPUT_H
+
+/*
+** NODES NUMBERS DEFINE
+ */
+
+# define SO_NODE_NB "# number of nodes            ="
+# define SO_PIPE_NB "# number of pipes            ="
+# define SO_INACT_PIPE_NB "# number of inactive pipes   ="
+# define SO_MVMT_NB "# number of movements        ="
+
+#endif
