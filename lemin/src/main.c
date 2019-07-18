b2c421b835fd32fa392e9217faba86995789e38d
Cesar <cesar@iMac-de-famille.local>
2019-07-18 13:51:37 +0200

    starting to work on an output

--- lemin/src/main.c
+++ lemin/src/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cgiron <cgiron@student.42.fr>              +#+  +:+       +#+        */
+/*   By: cesar <cesar@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/02 11:35:44 by cgiron            #+#    #+#             */
-/*   Updated: 2019/07/12 15:16:09 by cgiron           ###   ########.fr       */
+/*   Updated: 2019/07/17 11:19:40 by cesar            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,5 +24,17 @@ int			main(int  argc, char **argv)
 		return(1);
 	ft_init_mstr(mstr);
 	parser(mstr);
+	solver(mstr);
+//
+	int magic[3];
+	magic[0] = mstr->ants_nb;
+	magic[1] = mstr->adjacency_mtx[mstr->start->node_number][A_LINKS_NB];
+	magic[2] = mstr->adjacency_mtx[mstr->end->node_number][A_LINKS_NB];
+	ft_print_test(mstr, DEBUG_PRINT_TEST_RESULT);
+	printf("max flow options -%d -%d -%d\n", magic[0],magic[1],magic[2]);
+	printf("\n\nmax flow -%d\n", mstr->magic_number);
+	printf("final flow -%d\n", mstr->nb_solutions);
+	printf("amount of killed pipe - %d\n", mstr->killed);
+//^//
 	return (0);
 }
--- lemin/src/parser.c
+++ lemin/src/parser.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   parser.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cgiron <cgiron@student.42.fr>              +#+  +:+       +#+        */
+/*   By: cesar <cesar@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/02 14:35:16 by cgiron            #+#    #+#             */
-/*   Updated: 2019/07/12 16:39:17 by cgiron           ###   ########.fr       */
+/*   Updated: 2019/07/17 11:19:30 by cesar            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,17 +38,4 @@ void		parser(t_master *mstr)
 	ft_debug_storage_print(mstr->storage_start, mstr->lines_nb, DEBUG_PRINT_STORAGE);
 	printf("collisions : %d\n", mstr->dico->collisions);
 	printf("start: %d and end %d\n", mstr->start->node_number, mstr->end->node_number);
-//^//
-	solver(mstr);
-//
-	int magic[3];
-	magic[0] = mstr->ants_nb;
-	magic[1] = mstr->adjacency_mtx[mstr->start->node_number][A_LINKS_NB];
-	magic[2] = mstr->adjacency_mtx[mstr->end->node_number][A_LINKS_NB];
-	ft_print_test(mstr, DEBUG_PRINT_TEST_RESULT);
-	printf("max flow options -%d -%d -%d\n", magic[0],magic[1],magic[2]);
-	printf("\n\nmax flow -%d\n", mstr->magic_number);
-	printf("final flow -%d\n", mstr->nb_solutions);
-	printf("amount of killed pipe - %d\n", mstr->killed);
-//^//
 }
