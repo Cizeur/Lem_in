2fd8c8bc84eed8afbcd5415c0bcb7344dbe5bf2e
Cizeur <cgiron@e1r1p14.42.fr>
2019-08-03 16:16:39 +0200

    going from -g to -g3

--- shared_val.mk
+++ shared_val.mk
@@ -6,7 +6,7 @@
 #    By: cgiron <cgiron@student.42.fr>              +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2019/06/19 09:36:56 by cgiron            #+#    #+#              #
-#    Updated: 2019/06/19 12:49:31 by cgiron           ###   ########.fr        #
+#    Updated: 2019/08/03 16:02:18 by cgiron           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -21,4 +21,4 @@ vpath %.o $(SHARE_OBJ_DIR)
 
 SHARE_FLAGS				:= -Wall -Wextra -Werror -O3 -ffreestanding -fno-builtin -flto
 SHARE_FLAGS				+= -I $(SHARE_INC_DIR)
-SHARE_DEBUG				:= -O0 -g
+SHARE_DEBUG				:= -O0 -g3
