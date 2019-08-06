4f6c3605f2c1397feee7238cbdcbcf08b28a1be2
Cizeur <cgiron@e1r5p16.42.fr>
2019-08-06 17:48:05 +0200

    split of flags for debuging

--- shared_val.mk
+++ shared_val.mk
@@ -6,7 +6,7 @@
 #    By: cgiron <cgiron@student.42.fr>              +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2019/06/19 09:36:56 by cgiron            #+#    #+#              #
-#    Updated: 2019/08/03 16:02:18 by cgiron           ###   ########.fr        #
+#    Updated: 2019/08/06 17:39:28 by cgiron           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -19,6 +19,7 @@ vpath %.c $(SHARE_SRC_DIR)
 vpath %.h $(SHARE_INC_DIR)
 vpath %.o $(SHARE_OBJ_DIR)
 
-SHARE_FLAGS				:= -Wall -Wextra -Werror -O3 -ffreestanding -fno-builtin -flto
+SHARE_FLAGS				:= -Wall -Wextra -Werror
+SHARE_FLAGS				+= -O3 -ffreestanding -fno-builtin -flto
 SHARE_FLAGS				+= -I $(SHARE_INC_DIR)
 SHARE_DEBUG				:= -O0 -g3
