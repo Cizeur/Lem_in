0727a7b05ddc9b99cb5ad1b19efb5ca6f46486b7
Cizeur <cgiron@e1r5p16.42.fr>
2019-08-07 14:57:47 +0200

    options

---
+++ lemin/includes/lemin_options.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   lemin_options.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cgiron <cgiron@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/08/07 09:55:00 by cgiron            #+#    #+#             */
+/*   Updated: 2019/08/07 11:13:28 by cgiron           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LEMIN_OPTIONS_H
+# define LEMIN_OPTIONS_H
+
+# define O_USAGE "USAGE : ./lemin < [map_file]\n"
+
+/*
+** OPTIONS
+*/
+
+#define O_VISU "-v"
+#define O_VISU_FULL "-vu"
+#define O_OUTPUT_DEACT "-d"
+#define O_OUTPUT_JUST_SOL "-s"
+#define O_ANT_MOD "-a"
+
+#endif
