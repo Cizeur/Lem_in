95b02f0b96cbb1d6ff08d4bce374ea536a22b88d
Cizeur <cgiron@e1r5p16.42.fr>
2019-08-07 17:38:01 +0200

    error on double start or double end

--- lemin/src/ft_parser_apply_command.c
+++ lemin/src/ft_parser_apply_command.c
@@ -6,7 +6,7 @@
 /*   By: cgiron <cgiron@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/09 08:34:51 by cgiron            #+#    #+#             */
-/*   Updated: 2019/08/06 10:35:21 by cgiron           ###   ########.fr       */
+/*   Updated: 2019/08/07 17:37:19 by cgiron           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,17 @@ void		ft_parser_apply_command(t_master *mstr, t_line_info *entry)
 	else if (entry->type == END)
 		mstr->command_line = COM_END;
 	else if (mstr->command_line == COM_START && entry->type == NODE)
+	{
+		if (mstr->start)
+			ft_exit(DOUBLE_START, mstr);
 		mstr->start = entry;
+	}
 	else if (mstr->command_line == COM_END && entry->type == NODE)
+	{
+		if (mstr->end)
+			ft_exit(DOUBLE_START, mstr);
 		mstr->end = entry;
+	}
 	else if ((mstr->command_line == COM_START || mstr->command_line == COM_END)
 				&& entry->type != NODE)
 		ft_exit(WRONG_POSITION_FOR_START_END, mstr);
