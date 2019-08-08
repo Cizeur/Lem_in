59a2ef16822e8a71434e678b38f365f3c0da3f67
Cizeur <cgiron@e1r5p16.42.fr>
2019-08-08 18:32:25 +0200

    corrected turn counter

--- lemin/src/ft_output_explained_turns.c
+++ lemin/src/ft_output_explained_turns.c
@@ -6,7 +6,7 @@
 /*   By: cgiron <cgiron@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/08 14:49:41 by cgiron            #+#    #+#             */
-/*   Updated: 2019/08/08 17:25:45 by cgiron           ###   ########.fr       */
+/*   Updated: 2019/08/08 18:28:35 by cgiron           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,8 @@
 #include "output_explained.h"
 #include "libft/libft.h"
 
-void			ft_output_explained_turns(t_master *mstr, int ants, int turns)
+void			ft_check_enough_ants(t_master *mstr, int ants)
 {
-	if (mstr->output_type != OUTPUT_EXPLAINED_SHORT
-		&& mstr->output_type != OUTPUT_EXPLAINED)
-		return ;
 	if (!ants)
 		ft_output_putstr(
 			"\n\e[33mNOT ENOUGH ANTS FOR ACTIVATION\e[0m\n\n", mstr);
@@ -32,11 +29,35 @@ void			ft_output_explained_turns(t_master *mstr, int ants, int turns)
 			ft_max((mstr->ants - ants + 1), mstr->final_flow + 1), mstr);
 		ft_output_putstr("\e[33m REQUIRED \e[0m\n", mstr);
 	}
-	else if (ants == -1 && !mstr->final_flow)
+}
+
+void			ft_output_explained_turns(t_master *mstr, int ants, int turns)
+{
+	if (mstr->output_type != OUTPUT_EXPLAINED_SHORT
+		&& mstr->output_type != OUTPUT_EXPLAINED)
+		return ;
+	ft_check_enough_ants(mstr, ants);
+	if (ants == -1 && !mstr->final_flow)
 	{
 		ft_output_putstr("SOLUTION in : \e[32m", mstr);
 		ft_output_putnbr(turns, mstr);
 		ft_output_putstr("\e[0m turns\n", mstr);
+	}
+	else if (ants == -1 && mstr->final_flow && turns > mstr->turn_counter)
+	{
+		ft_output_putstr("SOLUTION SAVED : \e[32m", mstr);
+		ft_output_putnbr(turns + 1, mstr);
+		ft_output_putstr("\e[0m TURNS vs \e[32m", mstr);
+		ft_output_putnbr(mstr->turn_counter, mstr);
+		ft_output_putstr("\e[0m FOR PREVIOUS \e[0m\n", mstr);
+	}
+	else if (ants == -1 && mstr->final_flow && turns <= mstr->turn_counter)
+	{
+		ft_output_putstr("SOLUTION TRASHED : \e[32m", mstr);
+		ft_output_putnbr(turns, mstr);
+		ft_output_putstr("\e[0m TURNS vs \e[32m", mstr);
+		ft_output_putnbr(mstr->turn_counter, mstr);
+		ft_output_putstr("\e[0m FOR PREVIOUS \e[0m\n", mstr);
 	}
 	ft_output_buffer_flush(mstr);
 }
